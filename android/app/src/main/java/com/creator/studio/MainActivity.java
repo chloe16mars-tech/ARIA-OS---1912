35fcfa796bec1e46fae1ef9a3b48590af2e13709
DiyombiMeaGhislain <diyombimea@gmail.com>
2026-04-25 13:10:37 +0200

    feat: configure Vercel deployment and implement Android WebView permission handling for camera and audio

--- android/app/src/main/java/com/creator/studio/MainActivity.java
+++ android/app/src/main/java/com/creator/studio/MainActivity.java
@@ -1,5 +1,22 @@
 package com.creator.studio;
 
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Optimisation pour la caméra et le micro dans le WebView
+        this.bridge.getWebView().setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                request.grant(request.getResources());
+            }
+        });
+    }
+}
